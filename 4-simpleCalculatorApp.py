ec11fabe897d182b4171809c8cd78a3e997e0628
Samueldnz <samuelsd@dcc.ufrj.br>
2026-03-28 16:18:59 -0300

    update: build a simple calculator app part II, creating main calculator functions

--- 4-simpleCalculatorApp.py
+++ 4-simpleCalculatorApp.py
@@ -8,8 +8,24 @@ e = Entry(root, width=45, borderwidth=5)
 e.grid(row=0, column=0, columnspan=3, padx=10, pady=10)
 
 def button_click(number):
+    current = e.get()
     e.delete(0, END)
-    e.insert(0, number)
+    e.insert(0, str(current) + str(number))
+
+def clear():
+    e.delete(0, END)
+
+def add():
+    first_number = e.get()
+    global f_num
+    f_num = int(first_number)
+    e.delete(0, END)
+
+def equal():
+    second_number = e.get()
+    e.delete(0, END)
+    e.insert(0, f_num + int(second_number))
+    
 
 button_1 = Button(root, text=1, padx=40, pady=20, command=lambda: button_click(1))
 button_2 = Button(root, text=2, padx=40, pady=20, command=lambda: button_click(2))
@@ -22,9 +38,9 @@ button_8 = Button(root, text=8, padx=40, pady=20, command=lambda: button_click(8
 button_9 = Button(root, text=9, padx=40, pady=20, command=lambda: button_click(9))
 button_0 = Button(root, text=0, padx=40, pady=20, command=lambda: button_click(0))
 
-button_sum = Button(root, text="+", padx=39, pady=20)
-button_equal = Button(root, text="=", padx=91, pady=20)
-button_clear = Button(root, text="C", padx=91, pady=20)
+button_sum = Button(root, text="+", padx=39, pady=20, command=add)
+button_equal = Button(root, text="=", padx=91, pady=20, command=equal)
+button_clear = Button(root, text="C", padx=91, pady=20, command=clear)
 
 button_1.grid(row=3, column=0)
 button_2.grid(row=3, column=1)
