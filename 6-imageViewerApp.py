86e551e1d39325383d649328922a45f3350db337
Samueldnz <samuelsd@dcc.ufrj.br>
2026-04-01 12:23:54 -0300

    update: adding a status bar

--- 6-imageViewerApp.py
+++ 6-imageViewerApp.py
@@ -13,6 +13,8 @@ my_img6 = ImageTk.PhotoImage(Image.open("imagesApp/zoro.jpg"))
 
 image_list = [my_img1, my_img2, my_img3, my_img4, my_img5, my_img6] # simple list
 
+status = Label(root, text="Image 1 of " + str(len(image_list)), bd=1, relief=SUNKEN, anchor=E)
+
 my_label = Label(image=my_img1)
 my_label.grid(row=0, column=0, columnspan=3) # columnspan is about how long the thing will be. For exmaple, in this case, the imagem is in column 0 but expand until column 2
 
@@ -34,6 +36,9 @@ def forward(image_number):
     button_back.grid(row=1, column=0)
     button_forward.grid(row=1, column=2)
 
+    status = Label(root, text="Image " + str(image_number) + " of " + str(len(image_list)), bd=1, relief=SUNKEN, anchor=E)
+    status.grid(row=2, column=0, columnspan=3, sticky=W+E)
+
 def back(image_number):
     global my_label
     global vforward
@@ -53,6 +58,9 @@ def back(image_number):
     button_back.grid(row=1, column=0)
     button_forward.grid(row=1, column=2)
 
+    status = Label(root, text="Image " + str(image_number) + " of " + str(len(image_list)), bd=1, relief=SUNKEN, anchor=E)
+    status.grid(row=2, column=0, columnspan=3, sticky=W+E)
+
 
 button_back = Button(root, text="<<", command=back)
 button_exit = Button(root, text="Exit Program", command=root.quit)
@@ -60,6 +68,8 @@ button_forward = Button(root, text=">>", command=lambda: forward(2))
 
 button_back.grid(row=1, column=0)
 button_exit.grid(row=1, column=1)
-button_forward.grid(row=1, column=2)
+button_forward.grid(row=1, column=2, pady=10)
+status.grid(row=2, column=0, columnspan=3, sticky=W+E)
 
+# there somekind of direction system, like north and south. We can strech using sticky
 root.mainloop()
